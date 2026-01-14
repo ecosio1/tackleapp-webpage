40284deb00ccff313c8939748f431be16b2d0f34
ecosio1 <massagebyerikag@gmail.com>
2026-01-14 16:20:15 -0500

    Update blog grid to 4-column layout for desktop

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -125,9 +125,9 @@ export default async function BlogIndexPage({ searchParams }: BlogIndexPageProps
           )}
         </div>
 
-        {/* All Posts - Clean Card Grid */}
+        {/* All Posts - 4-Column Card Grid */}
         {blogPosts.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-16">
             {blogPosts.map((post) => (
               <ModernBlogCard
                 key={post.slug}
